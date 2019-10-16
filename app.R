a5a0cc170e22aa39f7839e48afeffc6fbee8501e
normur <54540112+normur@users.noreply.github.com>
2019-10-16 12:17:11 -0400

    API Fixed, leaflet proxy in process

--- app.R
+++ app.R
@@ -19,13 +19,15 @@ library(plotly)
 #Source: https://hifld-geoplatform.opendata.arcgis.com/search?groupIds=c779ef9b8468494fa2dbf7f573d61547
 
 # ups_locations <- readOGR("https://opendata.arcgis.com/datasets/d5c185658ec74c009ad956a92c50c58d_0.geojson")
-# dhl_locations <- readOGR("https://opendata.arcgis.com/datasets/01e20444878040278d4d99d0bbe95654_0.geojson")
-# fedex_locations <- readOGR("https://opendata.arcgis.com/datasets/13df698324c24807bc68ba7ac4f433cd_0.geojson")
-
-ups <- data.frame(ups_locations@data)
+# # dhl_locations <- readOGR("https://opendata.arcgis.com/datasets/01e20444878040278d4d99d0bbe95654_0.geojson")
+# # fedex_locations <- readOGR("https://opendata.arcgis.com/datasets/13df698324c24807bc68ba7ac4f433cd_0.geojson")
+# 
+# ups <- data.frame(ups_locations@data)
 # ups <- subset(ups, STATE == c("NY"))  # Filter by Northeast
 
+state_list <- sort(jsonlite::fromJSON("https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/UPS_Facilities/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=STATE&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=true&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=")$features$attributes$STATE)
 
+ups_type <- sort(jsonlite::fromJSON("https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/UPS_Facilities/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=NAME&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=true&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=")$features$attributes$NAME)
 
 header <- dashboardHeader(title = "UPS Shipping Locations", 
                           titleWidth = 450)
@@ -44,44 +46,43 @@ sidebar <- dashboardSidebar(
         
         pickerInput( inputId = "stateSelect",
                     label =  "Select State:",
-                    choices = sort(levels(ups$STATE)),
+                    choices = state_list,
                     options = list(`actions-box` = TRUE),
                     multiple = T, 
-                    selected = sort(levels(ups$STATE))),
+                    selected = c("MA")),
       
-        
         # select Type of UPS Location--------------------------
         
         pickerInput(inputId = "typeSelect", 
                     label = "Select Type of UPS Service Location", 
-                    choices = sort(levels(ups$NAME)),
+                    choices = ups_type,
                     options = list(`actions-box` = TRUE),
                     multiple = T, 
-                    selected = sort(levels(ups$NAME)))
+                    selected =  c("UPS DROP BOX")))
+
+        # NEED ANOTHER INPUt!!!!!!!!!!!!!!!!!!!!!!!
         
-        # Download Button for Data -------------------------------
+        # Download Button for Data ------------------------------- #####NEED! 
         
         # downloadButton("downloadData", "Download Selected Data")
         
-
-)
 
 body <- dashboardBody(theme = shinytheme("flatly"),
               tabItems(
               # Data Table Page -----------------------------------
                   tabItem("table",
                     box(title = "Data Table:", 
-                    DT::dataTableOutput(outputId = "datatable"))
+                    DT::dataTableOutput(outputId = "datatable"), width = 12)
                                 ),
               # Leaflet Map 
                   tabItem("map",
-                          title = "Nora's Map",
+                          title = "Map of UPS Locations",
                           leafletOutput("mapPlot")
                       ),
               # PLot Page
               tabItem("plot",
-                      box(title = "Plot 1",
-                          plotlyOutput("histo")), width = 10)
+                      box(title = "Histogram: Plot 1",
+                          plotlyOutput("histo")), width = 12)
               ))
 
 
@@ -89,21 +90,60 @@ ui <- dashboardPage( header, sidebar, body)
 
 server <- function(input, output) {
   
+  
+  ############ Get Data API CALL BY STATE and NAME (inputs in link) #####################################
+  
+awesome_reactive_data <-  reactive({ 
+  
+  df<- readOGR("https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/UPS_Facilities/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=NAME%2C+STATE%2C%27input$stateSelect%27%2C+BUSINESS_NAME%2C+CITY%2C+CENSUS_CODE%2C+PHONE%2C+LATITUDE%2C+LONGITUDE&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=standard&f=pgeojson&token=") 
+ 
+  df <- data.frame(df@data)
+  df
+})
+  
   # Filter Data by state and Type of Serivce
   state_serviceSubset <- reactive({
     req(input$stateSelect, input$typeSelect)
-    data_subset <- ups[ups$STATE %in% input$stateSelect & (ups$NAME %in% input$typeSelect),]
+    data_subset <- awesome_reactive_data()[awesome_reactive_data()$STATE %in% input$stateSelect &
+                                             (awesome_reactive_data()$NAME %in% input$typeSelect),]
     data_subset
-    
+
   })
   ########################### MAP #######################################
   
+  # Create Base Map
+  
   output$mapPlot <- renderLeaflet({
-    leaflet(state_serviceSubset())%>%
+    leaflet()%>%
       addTiles()%>%
-      addProviderTiles("OpenStreetMap.BZH", group = "BZH")%>%
-      addAwesomeMarkers(icon = awesomeIcons(icon ="envelope", library = "glyphicon"))
+      addProviderTiles("OpenStreetMap.BZH", group = "BZH")})
+      
+
+  
+  # Now Add Layers of Points for Location Type-------------------------------------------
+  
+
+  observe({
+    
+    #Make Palette
+    pal <- colorFactor(palette = "Paired", domain = ups_type)
+    
+    # Make Icons
+    icons <- awesomeIcons(icon = "envelope",iconColor = 'blue',library = 'glyphicon')
+    
+    leafletProxy("mapPlot", data = state_serviceSubset())%>%
+      clearGroup(group = state_serviceSubset()$NAME) %>%
+      addAwesomeMarkers(lng = LONGITUDE,
+                        lat = LATITUDE,
+                        icon = icons, group = state_serviceSubset()$NAME, ~pal(NAME))%>%
+      addLayersControl(
+        overlayGroups = c(levels(state_serviceSubset()$NAME)),
+        options = layersControlOptions(collapsed = FALSE))%>%
+      addLegend("bottomright", pal = pal, values = ~NAME,
+                title = "Type of UPS Location")
   })
+  
+
   
   ########################### DATA TABLE ################################ 
   
@@ -122,7 +162,7 @@ server <- function(input, output) {
     
     g <- ggplot(state_serviceSubset(), aes(STATE)) + 
       geom_bar(aes(fill=NAME), width = 0.5) + 
-      theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+      theme(axis.text.x = element_text(angle=85, vjust=0.6)) + 
       labs(title="UPS Locations", 
            subtitle="Type of Shipping Location by State", x = "State") +
       theme(legend.title = element_blank()) + scale_fill_brewer(palette = "Paired")
